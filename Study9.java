739cedaaa4d0f16d121dca7e28250d31646b951c
makoto <1027.mako.000@gmail.com>
2021-02-14 15:53:43 +0900

    update Study9

--- Study9.java
+++ Study9.java
@@ -1,22 +1,30 @@
 package javaStudy;
 
+import java.util.Collections;
+
 public class Study9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String a =System.lineSeparator();
 		for (int i = 1; i < 10; i++) {
-			for (int n = 1; n < 10; n++) {
-				int in = i * n;
-				if (in % 5 == 0) {
+			for(int j = 1; j < 10; j++) {
+				int k = i * j;
+				if ((i * j)% 5 == 0) {
 					System.out.print("■　");
-				}else if (in <= 7 || in >= 54) {
-					System.out.print("□　");
+				}else if((i == 2 || j == 2) && k > 7 && k < 13) {
+					System.out.print("■　");
+				}else if((i == 3 || j == 3) && k > 8 && k < 22) {
+					System.out.print("■　");
+				}else if((i == 4 || j == 4) && k > 7 && k < 33) {
+					System.out.print("■　");
+				}else if((i == 6 || j == 6 || j == 7) && k > 35 && k < 50) {
+					System.out.print("■　");
 				}else {
-					System.out.print("■　");
+					System.out.print("□　");
 				}
 			}
-			System.out.println(a);
+			System.out.println("");
 		}
 	}
 }
+
