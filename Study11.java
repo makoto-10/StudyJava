263a8d91b9ffbb877c509c86bd29c1c3854cf168
makoto <1027.mako.000@gmail.com>
2021-02-21 21:21:09 +0900

    creat Stydy11.java

---
+++ Study11.java
@@ -0,0 +1,24 @@
+package javaStudy;
+
+import java.util.Random;
+
+import java.util.Scanner;
+
+public class Study11 {
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//画面表示
+		System.out.println("◆High&Lowゲーム◆");
+		System.out.println();
+		System.out.println("～ルール～");
+		System.out.println("①AとBの2つの数字がランダムに生成される。");
+		System.out.println("②生成される数字はA・Bいずれも1～10の中から1つ選ばれる。");
+		System.out.println("③Aの数字を先に確認したあと、Bの数字を予想する。");
+		System.out.println("④A+B=11以上の予想なら「1」、10以下の予想なら「2」を記入する。");
+		System.out.println("⑤実行後、Bの数字及び予想の結果が表示される。");
+	}
+
+}
