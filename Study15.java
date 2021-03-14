1257bf417ef88437fe8544cfb230f4d53e00a14b
makoto <1027.mako.000@gmail.com>
2021-03-15 07:21:41 +0900

    create Study15.java

---
+++ Study15.java
@@ -0,0 +1,15 @@
+package javaStudy;
+
+public class Study15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int result = total(5,7);
+		System.out.println(result);
+	}
+	
+	public static int total(int a, int b) {
+		return a + b;
+	}
+
+}
