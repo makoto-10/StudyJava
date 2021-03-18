a697cc83bef1bd85459778ea04970a93b1c08858
makoto <1027.mako.000@gmail.com>
2021-03-18 15:20:09 +0900

    update Study16.java

--- Study16.java
+++ Study16.java
@@ -11,9 +11,23 @@ public class Study16 {
 		System.out.println(name + "さんの身長は" + weight + "kgです");
 		double bmi = bmi(height,weight);
 		System.out.println(name + "さんのBMI値は" + String.format("%.1f",bmi) + "kgです");
+		
+		String bmiResult = branch(bmi);
+		System.out.println(bmiResult);
 	}
 	
 	public static double bmi(double height, double weight) {
 		return weight / height / height;
-	}	
+	}
+	
+	
+	public static String branch(double bmi) {
+		if(bmi < 25 && bmi >= 18.5) {
+			return "標準です。";
+		}else if(bmi >= 25) {
+			return "肥満です。";
+		}else {
+			return "低体重です。";
+		}
+	}
 }
